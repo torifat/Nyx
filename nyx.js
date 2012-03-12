f491bee0164aa5c9f6209431f483eb54a9986f09
Rifat Nabi <to.rifat@gmail.com>
2012-03-12 09:38:07 +0600

    Rearranged
    + io added to track the instance
    + forEach polyfill added
    + Selene updated
    
    Signed-off-by: Rifat Nabi <to.rifat@gmail.com>

--- nyx.js
+++ nyx.js
@@ -6,30 +6,64 @@
         return Object.create(Chaos).init(selector, context);
     };
 
+    // Cow - http://en.wikipedia.org/wiki/Io_(mythology)
+    var io;
+
     // Void - http://en.wikipedia.org/wiki/Chaos_(cosmogony)
     var Chaos = {
-        // constructor: JQL,
-        version: '1.0',
-        length: 0,
+        version: "1.0",
+        selector: "",
         context: window.document,
+        length: 0,
         init: function(selector, context){
+            io = this;
             return Selene.parse(selector, context);
         },
-        splice: function(){}
+        push: Array.prototype.push,
+        splice: [].splice
     };
 
     // Moon - http://en.wikipedia.org/wiki/Selene
     var Selene = {
-        parse: function(selector){
+        parse: function(selector, context){
             // Empty
             if(!selector) {
-                return Chaos;
+                return io;
             }
             // DOM Elements
             if(selector.nodeType) {
-                Chaos.context = Chaos[0] = selector;
-                return Chaos;
+                io.context = io[0] = selector;
+                return io;
             }
+            // CSS Selector
+            if(typeof selector === "string") {
+                if(typeof context === "undefined") {
+                    context = io.context;
+                } else {
+                    io.context = context;
+                }
+                return this.css(selector, context);
+            }
+            // Invalid Selector
+            throw new Error("Invalid Selector");
+        },
+        css: function(selector, context){
+            selector = io.selector = this.clean(selector);
+            var items = selector.split(/\s+/),
+                cur = [];
+            // Id selector
+            if(items[0][0] === "#") {
+                var item = items.shift();
+                cur[0] = document.getElementById(item.substring(1));
+                if(!items.length) {
+                    io[0] = cur[0];
+                    io.length = 1;
+                    return io;
+                }
+            }
+        },
+        clean: function(selector){
+            return selector.substring(selector.lastIndexOf("#"));
         }
     };
 
@@ -38,11 +72,28 @@
     };
 
     // Polyfill for Old Browsers
-    if(typeof Object.create !== 'function') {
+    if(typeof Object.create !== "function") {
         Object.create = function(o) {
             function F() {}
             F.prototype = o;
             return new F();
+        };
+    }
+    if(typeof Array.prototype.forEach !== "function") {
+        Array.prototype.forEach = function(fun, thisp) {
+            if (this === void 0 || this === null) {
+                throw new TypeError();
+            }
+            var t = Object(this);
+            var len = t.length >>> 0;
+            if (typeof fun !== "function") {
+                throw new TypeError();
+            }
+            for (var i = 0; i < len; i++) {
+                if (i in t) {
+                    fun.call(thisp, t[i], i, t);
+                }
+            }
         };
     }
 
